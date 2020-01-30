f03ea258c1d9de9bb52a87ba999a37899c948db0
Zachary Onisick <Zonisick@uncc.edu>
2020-01-30 18:57:57 +0000

    RubyHases part- 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,7 +1,15 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
+    counter = 0
+    contacts.each do |key, value|
+        if emails[counter] != nil
+            contacts[key] = emails[counter]
+        else
+            contacts[key] = ""
+        end
+        counter += 1
+    end
 end
 
 # Part II
@@ -13,3 +21,6 @@ end
 def hash_2_array contacts
     # YOUR CODE HERE
 end
+
+array_2_hash(["bobsmith@example.com","sallyfield@example.com","markdole@example.com"], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''})
+
