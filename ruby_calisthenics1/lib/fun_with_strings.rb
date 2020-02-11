22154f4181a9061671e4efc2d0b8cda86d4c0177
Zachary Onisick <Zonisick@uncc.edu>
2020-02-11 22:26:54 +0000

    im getting errors

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,12 +1,48 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    if self.reverse.upcase.gsub(/[^0-9A-Z]+/,"") == self.upcase.gsub(/[^0-9A-Z]+/,"")
+      return true
+    end
+    return false
   end
+  
   def count_words
-    # your code here
+    tempList = self.split(" ")
+    tempHash = Hash.new
+    tempList.each do |x|
+      if x.downcase.gsub(/[^0-9a-z]+/,"") == ""
+        next
+      end
+      if !tempHash.include? x.downcase.gsub(/[^0-9a-z]+/,"")
+        tempHash[x.downcase.gsub(/[^0-9a-z]+/,"")] = 1
+      else
+        tempHash[x.downcase.gsub(/[^0-9a-z]+/,"")] += 1
+      end
+    end
+    return tempHash
   end
+  
   def anagram_groups
-    # your code here
+    ansArray = []
+    found = false
+    tempArray = self.split(" ")
+    for i in 0..tempArray.length-1 do
+      tempString = tempArray[i].split("")
+      tempString = tempString.sort
+      for j in i + 1..tempArray.length-1 do
+        tempString2 = tempArray[j].split("")
+        tempString2 = tempString2.sort
+        if tempString == tempString2
+          ansArray.append([tempArray[i], tempArray[j]])
+          found = true
+        end
+      end
+      if found == false
+        ansArray.append([tempArray[i]])
+      end
+      found = false
+    end
+    return ansArray
   end
 end
 
@@ -15,3 +51,5 @@ end
 class String
   include FunWithStrings
 end
+
+puts "scream cars for four scar creams".anagram_groups
