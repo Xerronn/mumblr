c8a87220c0ab6cf4a316f9532e0841407080b7cd
kharpe16 <kharpe16@uncc.edu>
2020-05-07 04:09:46 +0000

    Created rspec test cases

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -1,16 +1,52 @@
 require 'rails_helper.rb'
 
 feature"Blogger adds an article" do
-    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
-        visit articles_path
-        expect(page).to have_content("Listing articles")
-        click_link "New article"
-        expect(page).to have_content("New Article")
-        expect(page).to have_field("Title")
-        expect(page).to have_field("Text")
+
+     scenario "Blogger successfully create an account" do
+        visit signup_path
+        fill_in "Name", with:"User"
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
     end
     
+    scenario "Blogger successfully logins into their account" do
+        visit signup_path
+        fill_in "Name", with:"User"
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
+        visit login_path
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Users")
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
+    end 
+    
     scenario "Blogger successfully creates a new article" do
+        visit signup_path
+        fill_in "Name", with:"User"
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
+        visit login_path
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Users")
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
         visit new_article_path
         expect(page).to have_content("New Article")
         fill_in "Title", with:"New Capybara Article"
@@ -18,37 +54,66 @@ feature"Blogger adds an article" do
         click_button "Create Article"
         expect(page).to have_content("New Capybara Article")
         expect(page).to have_content("This is a new Capybara article")
-    end
+    end    
     
-    scenario "Blogger successfully edits an article" do
+    scenario "Blogger successfully creates a comment" do 
+        visit signup_path
+        fill_in "Name", with:"User"
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
+        visit login_path
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Users")
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
         visit new_article_path
         expect(page).to have_content("New Article")
         fill_in "Title", with:"New Capybara Article"
         fill_in "Text", with:"This is a new Capybara article"
         click_button "Create Article"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+        fill_in "Commenter", with:"User"
+        fill_in "Body", with: "This is a Capybara Comment"
+        click_button "Create Comment"
+        expect(page).to have_content("User")
+        expect(page).to have_content("This is a Capybara Comment")
+    end
+    
+    scenario "Blogger successfully edits a comment" do
+        visit signup_path
+        fill_in "Name", with:"User"
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
+        visit login_path
+        fill_in "Email", with: "user@uncc.edu"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Users")
+        expect(page).to have_content("User")
+        expect(page).to have_content("user@uncc.edu")
         visit new_article_path
+        expect(page).to have_content("New Article")
         fill_in "Title", with:"New Capybara Article"
         fill_in "Text", with:"This is a new Capybara article"
         click_button "Create Article"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
         visit edit_article_path(Article.find(1))
         fill_in "Title", with:"Edited Capybara Article"
         fill_in "Text", with:"This is an edited Capybara article"
         click_button "Update Article"
         expect(page).to have_content("Edited Capybara Article")
         expect(page).to have_content("This is an edited Capybara article")
-    
     end
-    
-    scenario "Blogger successfully creates a comment" do
-        visit new_article_path
-        fill_in "Title", with:"New Capybara Article"
-        fill_in "Text", with:"This is a new Capybara article"
-        click_button "Create Article"
-        fill_in "Commenter", with:"Test"
-        fill_in "Body", with:"This is a test comment"
-        click_button "Create Comment"
-        expect(page).to have_content("Test")
-        expect(page).to have_content("This is a test comment")
-    end
-    
 end
